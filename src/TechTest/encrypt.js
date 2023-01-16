ec0391af748b934712359d9941ee60bcbc769494
Michael Villaizan <96017264+MichaelVillaizan@users.noreply.github.com>
2023-01-16 18:53:17 -0500

    Delete src directory

--- src/TechTest/encrypt.js
+++
@@ -1,8 +0,0 @@
-export default async function digestMessage(message) {
-        const encoder = new TextEncoder();
-        const data = encoder.encode(message);
-        const hash = await crypto.subtle.digest('SHA-256', data);
-        return hash;
-}
-        
-        
